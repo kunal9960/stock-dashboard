79660772ae273d920ed5101f9c5d8477d17e6171
Kunal Dalvi <kunald9960@gmail.com>
2024-06-08 17:44:09 +0530

    Change for graph and title

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ from itertools import islice
 from datetime import datetime
 from plotly.subplots import make_subplots
 
-st.set_page_config(page_title="Stocks Dashboard", page_icon="📈", layout="wide")
+
+icon = "favicon.png"
+st.set_page_config(page_title="Stocks Dashboard", page_icon=icon, layout="wide")  # Page config
 st.html("styles.html")
 pio.templates.default = "plotly_white"
 
 
-def batched(iterable, n_cols):  # this is for rows and columns
+def batched(iterable, n_cols):  # This is for rows and columns
     if n_cols < 1:
         raise ValueError("n must be at least one")
     it = iter(iterable)
@@ -22,17 +24,17 @@ def batched(iterable, n_cols):  # this is for rows and columns
 
 
 @st.cache_data
-def _read_service_account_secret():
+def _read_service_account_secret():  # Most important, to load json -> toml file
     return json.loads(st.secrets["textkey"])
 
 
 @st.cache_resource
-def connect_to_gsheets():
+def connect_to_gsheets():  # Establishing airbyte connection with service account and excel
     s_acc = _read_service_account_secret()
     gsheets_connection = ab.get_source(
         "source-google-sheets",
         config={
-            "spreadsheet_id": "https://docs.google.com/spreadsheets/d/1bdhpSDG3uuF3NfLUDdZGVZoscxqSLLavmZzV9RYhrTc/edit",
+            "spreadsheet_id": st.secrets["stocks"],
             "credentials": {
                 "auth_type": "Service",
                 "service_account_info": json.dumps(s_acc),
@@ -44,7 +46,7 @@ def connect_to_gsheets():
 
 
 @st.cache_data
-def download_data(_gsheets_connection):
+def download_data(_gsheets_connection):  # Downloading data from airbyte
     airbyte_streams = _gsheets_connection.read()
 
     ticker_df = airbyte_streams["ticker"].to_pandas()
@@ -57,54 +59,8 @@ def download_data(_gsheets_connection):
     return ticker_df, history_dfs
 
 
-def display_overview(ticker_df):
-    def format_currency(val):
-        return "$ {:,.2f}".format(val)
-
-    def format_percentage(val):
-        return "{:,.2f} %".format(val)
-
-    def format_change(val):
-        return "color: red;" if (val < 0) else "color: green;"
-
-    def apply_odd_row_class(row):
-        return ["background-color: #f8f8f8" if row.name % 2 != 0 else "" for _ in row]
-
-    with st.expander("📊 Stocks Preview"):
-        styled_df = (
-            ticker_df.style.format(
-                {
-                    "last_price": format_currency,
-                    "change_pct": format_percentage,
-                }
-            )
-            .apply(apply_odd_row_class, axis=1)
-            .map(format_change, subset=["change_pct"])
-        )
-
-    st.dataframe(
-        styled_df,
-        column_order=[column for column in list(ticker_df.columns) if column not in [
-            "_airbyte_raw_id",
-            "_airbyte_extracted_at",
-            "_airbyte_meta",
-        ]
-                      ],
-        column_config={
-            "open": st.column_config.AreaChartColumn(
-                "Last 12 Months",
-                width="large",
-                help="Open Price for the last 12 Months",
-            ),
-        },
-        hide_index=True,
-        height=250,
-    )
-
-
 @st.cache_data
-def transform_data(ticker_df, history_dfs):
-    # Automatically infer the datetime format for ticker_df
+def transform_data(ticker_df, history_dfs):  # Automatically changes the datetime format for ticker_df
     ticker_df["last_trade_time"] = pd.to_datetime(
         ticker_df["last_trade_time"],
         infer_datetime_format=True,
@@ -133,59 +89,84 @@ def transform_data(ticker_df, history_dfs):
     return ticker_df, history_dfs
 
 
-def plot_candlestick(history_df):
-    f_candle = make_subplots(
-        rows=2,
-        cols=1,
-        shared_xaxes=True,
-        row_heights=[0.7, 0.3],
-        vertical_spacing=0.1,
-    )
-
-    f_candle.add_trace(
-        go.Candlestick(
-            x=history_df.index,
-            open=history_df["open"],
-            high=history_df["high"],
-            low=history_df["low"],
-            close=history_df["close"],
-            name="Dollars",
+def plot_sparkline(data):
+    fig_spark = go.Figure(
+        data=go.Scatter(
+            y=data,
+            mode="lines",
+            fill="tozeroy",
+            line_color="#6abfb7",
+            fillcolor="#d6edeb",
         ),
-        row=1,
-        col=1,
     )
-    f_candle.add_trace(
-        go.Bar(
-            x=history_df.index,
-            y=history_df["volume"],
-            name="Volume Traded"),
-        row=2,
-        col=1,
+    fig_spark.update_traces(hovertemplate="Price: $ %{y:.2f}")
+    fig_spark.update_xaxes(visible=False, fixedrange=True)
+    fig_spark.update_yaxes(visible=False, fixedrange=True)
+    fig_spark.update_layout(
+        showlegend=False,
+        plot_bgcolor="white",
+        height=50,
+        margin=dict(t=10, l=0, b=3, r=2, pad=0),
     )
-    f_candle.update_layout(
-        title="Stock Price Trends",
-        showlegend=True,
-        xaxis_rangeslider_visible=False,
-        yaxis1=dict(title="OHLC"),
-        yaxis2=dict(title="Volume"),
-        hovermode="x",
-    )
-    f_candle.update_layout(
-        title_font_family="Consolas",
-        title_font_color="#174C4F",
-        title_font_size=40,
-        font_size=20,
-        margin=dict(l=80, r=80, t=100, b=80, pad=0),
-        height=500,
-    )
-    f_candle.update_xaxes(title_text="Date", row=2, col=1)
-    f_candle.update_traces(selector=dict(name="Dollars"), showlegend=True)
-    return f_candle
+    return fig_spark
 
+
+def display_watchlist_card(ticker, symbol_name, last_price, change_pct, open):
+    with st.container(border=True):
+        st.html(f'<span class="watchlist_card"></span>')
+
+        tl, tr = st.columns([1, 1])
+        bl, br = st.columns([0.8, 1.3])
+
+        with tl:
+            st.html(f'<span class="watchlist_symbol_name"></span>')
+            st.markdown(f"{symbol_name}")
+
+        with tr:
+            st.html(f'<span class="watchlist_ticker"></span>')
+            st.markdown(f"{ticker}")
+            negative_gradient = float(change_pct) < 0
+            st.markdown(
+                f":{'red' if negative_gradient else 'green'}[{'▼' if negative_gradient else '▲'} {change_pct} %]"
+            )
+
+        with bl:
+            with st.container():
+                st.html(f'<span class="watchlist_price_label"></span>')
+                st.markdown(f"Current Value")
+
+            with st.container():
+                st.html(f'<span class="watchlist_price_value"></span>')
+                st.markdown(f"${last_price:.2f}")
+
+        with br:
+            fig_spark = plot_sparkline(open)
+            st.html(f'<span class="watchlist_br"></span>')
+            st.plotly_chart(
+                fig_spark, config=dict(displayModeBar=False), use_container_width=True
+            )
+
+
+def display_watchlist(ticker_df):
+    n_cols = 4
+
+    for row in batched(ticker_df.itertuples(), n_cols):
+        cols = st.columns(n_cols)
+        for col, ticker in zip(cols, row):
+            if ticker:
+                with col:
+                    display_watchlist_card(
+                        ticker.ticker,
+                        ticker.symbol_name,
+                        ticker.last_price,
+                        ticker.change_pct,
+                        ticker.open
+                    )
 
 
 @st.experimental_fragment
 def display_symbol_history(ticker_df, history_df):
+    st.write("<h2><b>🚀 <u>Period Performance Analysis</b></h2>", unsafe_allow_html=True)
     left_widget, right_widget, _ = st.columns([1, 1, 1.5])
 
     selected_ticker = left_widget.selectbox(
@@ -194,9 +175,9 @@ def display_symbol_history(ticker_df, history_df):
     )
 
     selected_period = right_widget.selectbox(
-        "⌛ Period",
+        "⏱️ Period",
         ("Week", "Month", "Trimester", "Year"),
-        2,
+        1,
     )
 
     history_df = history_dfs[selected_ticker]
@@ -221,7 +202,7 @@ def display_symbol_history(ticker_df, history_df):
         st.plotly_chart(f_candle, use_container_width=True)
 
     with right_indicator:
-        st.subheader("Period Metrics")
+        st.write("<h2 style='font-weight: bold;'>Period Metrics</h2>", unsafe_allow_html=True)
         l, r = st.columns(2)
 
         with l:
@@ -264,87 +245,113 @@ def display_symbol_history(ticker_df, history_df):
     st.dataframe(history_df)
 
 
+def plot_candlestick(history_df):
+    f_candle = make_subplots(
+        rows=2,
+        cols=1,
+        shared_xaxes=True,
+        row_heights=[0.7, 0.3],
+        vertical_spacing=0.1,
 
+    )
 
-
-def plot_sparkline(data):
-    fig_spark = go.Figure(
-        data=go.Scatter(
-            y=data,
-            mode="lines",
-            fill="tozeroy",
-            line_color="red",
-            fillcolor="pink",
+    f_candle.add_trace(
+        go.Candlestick(
+            x=history_df.index,
+            open=history_df["open"],
+            high=history_df["high"],
+            low=history_df["low"],
+            close=history_df["close"],
+            name="Dollars",
         ),
+        row=1,
+        col=1,
+
     )
-    fig_spark.update_traces(hovertemplate="Price: $ %{y:.2f}")
-    fig_spark.update_xaxes(visible=False, fixedrange=True)
-    fig_spark.update_yaxes(visible=False, fixedrange=True)
-    fig_spark.update_layout(
-        showlegend=False,
-        plot_bgcolor="white",
-        height=50,
-        margin=dict(t=10, l=0, b=0, r=0, pad=0),
+    f_candle.add_trace(
+        go.Bar(
+            x=history_df.index,
+            y=history_df["volume"],
+            name="Volume Traded"),
+        row=2,
+        col=1,
     )
-    return fig_spark
+    f_candle.update_layout(
+        title="Stock Price Trends",
+        showlegend=True,
+        xaxis_rangeslider_visible=False,
+        yaxis1=dict(title="OHLC"),
+        yaxis2=dict(title="Volume"),
+        hovermode="x",
+    )
+    f_candle.update_layout(
+        title_font_color="black",
+        title_font_size=38,
+        font_size=20,
+        margin=dict(l=0, r=0, t=80, b=98, pad=0),
+        height=520,
+    )
+    f_candle.update_xaxes(title_text="Date", row=2, col=1)
+    f_candle.update_traces(selector=dict(name="Dollars"), showlegend=True)
+    return f_candle
 
 
-def display_watchlist_card(ticker, symbol_name, last_price, change_pct, open):
-    with st.container(border=True):
-        st.html(f'<span class="watchlist_card"></span>')
+def display_overview(ticker_df):
+    def format_currency(val):
+        return "$ {:,.2f}".format(val)
 
-        tl, tr = st.columns([2, 1])
-        bl, br = st.columns([0.7, 1.1])
+    def format_percentage(val):
+        return "{:,.2f} %".format(val)
 
-        with tl:
-            st.html(f'<span class="watchlist_symbol_name"></span>')
-            st.markdown(f"{symbol_name}")
+    def format_change(val):
+        return "color: red;" if (val < 0) else "color: green;"
 
-        with tr:
-            st.html(f'<span class="watchlist_ticker"></span>')
-            st.markdown(f"{ticker}")
-            negative_gradient = float(change_pct) < 0
-            st.markdown(
-                f":{'red' if negative_gradient else 'green'}[{'▼' if negative_gradient else '▲'} {change_pct} %]"
+    def apply_odd_row_class(row):
+        return ["background-color: #f8f8f8" if row.name % 2 != 0 else "" for _ in row]
+
+    with st.expander("📈 Stocks Preview"):
+        styled_df = (
+            ticker_df.style.format(
+                {
+                    "last_price": format_currency,
+                    "change_pct": format_percentage,
+                }
             )
+            .apply(apply_odd_row_class, axis=1)
+            .map(format_change, subset=["change_pct"])
+        )
 
-        with bl:
-            with st.container():
-                st.html(f'<span class="watchlist_price_label"></span>')
-                st.markdown(f"Current Value")
-
-            with st.container():
-                st.html(f'<span class="watchlist_price_value"></span>')
-                st.markdown(f"$ {last_price:.2f}")
-
-        with br:
-            fig_spark = plot_sparkline(open)
-            st.html(f'<span class="watchlist_br"></span>')
-            st.plotly_chart(
-                fig_spark, config=dict(displayModeBar=False), use_container_width=True
-            )
-
-
-def display_watchlist(ticker_df):
-    n_cols = 4
-
-    for row in batched(ticker_df.itertuples(), n_cols):
-        cols = st.columns(n_cols)
-        for col, ticker in zip(cols, row):
-            if ticker:
-                with col:
-                    display_watchlist_card(
-                        ticker.ticker,
-                        ticker.symbol_name,
-                        ticker.last_price,
-                        ticker.change_pct,
-                        ticker.open
-                    )
+        st.dataframe(
+            styled_df,
+            column_order=[column for column in list(ticker_df.columns) if column not in [
+                "_airbyte_raw_id",
+                "_airbyte_extracted_at",
+                "_airbyte_meta",
+            ]
+                          ],
+            column_config={
+                "open": st.column_config.AreaChartColumn(
+                    "Last 12 Months",
+                    width="large",
+                    help="Open Price for the last 12 Months",
+                ),
+            },
+            hide_index=True,
+            height=250,
+            use_container_width=True,
+        )
 
 
-st.title("Stocks Dashboard")
+col1, col2 = st.columns([1, 2])
+with col1:
+    st.write("<h2><b>📈 <u>Stocks Dashboard</b></h2>", unsafe_allow_html=True)
+    st.write("<i>A stock dashboard that provides real-time data, performance charts, and key financial metrics for understanding the behaviour of companies.</i>", unsafe_allow_html=True)
+with col2:
+    st.image("stocks-header.gif", width=155)
+
 gsheets_connection = connect_to_gsheets()
 ticker_df, history_dfs = download_data(gsheets_connection)
+
 ticker_df, history_dfs = transform_data(ticker_df, history_dfs)
 
 display_watchlist(ticker_df)
