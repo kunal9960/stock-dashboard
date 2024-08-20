cf16cbbe873c5df53b1d13a7537121565a641964
Kunal Dalvi <kunald9960@gmail.com>
2024-08-20 18:30:03 +0530

    Update for sdk

--- main.py
+++ main.py
@@ -355,58 +355,6 @@ with col1:
 with col2:
     st.image("stock-header.gif", width=155)
 
-st.markdown(
-    "[![GitHub Badge](https://img.shields.io/badge/GitHub-181717?logo=github&logoColor=fff&style=flat)](https://github.com/kunal9960/stocks-dashboard)&nbsp;&nbsp;" +
-    "[![Streamlit Badge](https://img.shields.io/badge/Streamlit-FF4B4B?logo=streamlit&logoColor=fff&style=flat)](https://stock-dashboard-kunal.streamlit.app/)")
-
-
-ft = """
-<style>
-a:link , a:visited{
-color: #BFBFBF;  /* theme's text color hex code at 75 percent brightness*/
-background-color: transparent;
-text-decoration: none;
-}
-
-a:hover,  a:active {
-color: #0283C3; /* theme's primary color*/
-background-color: transparent;
-text-decoration: underline;
-}
-
-#page-container {
-  position: relative;
-  min-height: 10vh;
-}
-
-footer{
-    visibility:hidden;
-}
-
-.footer {
-position: relative;
-left: 0;
-top:150px;
-bottom: 0;
-width: 100%;
-background-color: transparent;
-color: #808080;
-text-align: left;
-}
-</style>
-
-<div id="page-container">
-
-<div class="footer">
-<p style='font-size: 1em;'>Made with <a style='display: inline; text-align: left;' href="https://streamlit.io/" target="_blank">Streamlit</a><br 'style= top:3px;'>
-with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/kunal9960" target="_blank"> by Kunal</a>
-<a style='display: inline; text-align: left;'>© Copyright 2024</a></p>
-</div>
-
-</div>
-"""
-st.write(ft, unsafe_allow_html=True)
-
 gsheets_connection = connect_to_gsheets()
 ticker_df, history_dfs = download_data(gsheets_connection)
 ticker_df, history_dfs = transform_data(ticker_df, history_dfs)
